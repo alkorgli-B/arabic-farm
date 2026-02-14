25e5cfe225c23f3c1d8ec375c73877c946c4a341
alkorgli-B <alkorgli26@gmail.com>
2026-02-14 16:55:11 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neonCyan: '#00ffcc',
+        neonBlue: '#0088FF',
+        neonPink: '#FF0055',
+        neonGold: '#FFD700',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
